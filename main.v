8d552e4f4dd09e4a8857d4225d9f1220b51d9c72
WatanukWolf <watanuki.wolf.2018@gmail.com>
2022-01-21 02:21:20 +0900

    [mod] main.v
    Use vweb to show parsed information

--- main.v
+++ main.v
@@ -1,13 +1,27 @@
 module main
 
+import vweb
+
+struct App {
+    vweb.Context
+}
+
 fn main() {
+    app := App{}
+    vweb.run(app, 8081)
+}
+
+pub fn (mut app App) index() vweb.Result {
     ipynb := parse_ipynb('./sample/sample.ipynb') or {
-	return
+	IPynb{}
     }
     for cell in ipynb.cells {
 	println('$cell.source')
     }
+    sources := ipynb.cells.map(it.source)
+    ecounts := ipynb.cells.map(it.execution_count)
     // println('$ipynb.metadata')
     println('')
+    return $vweb.html()
 }
 
