dc3d422ac4c78433289e6ff7d2c4b50255afd624
WatanukWolf <watanuki.wolf.2018@gmail.com>
2022-01-21 01:25:33 +0900

    [mod] main.v
    Change it into using the methods defined in the other files

--- main.v
+++ main.v
@@ -1,71 +1,7 @@
-import os
-import json
-
-struct CodeMirrorMode {
-    name string
-    version int
-}
-
-struct LangInfo {
-    name string
-    version string
-    mimetype string
-    codemirror_mode CodeMirrorMode
-    pygments_lexer string
-    nbconvert_exporter string
-    file_extension string
-}
-
-struct KernelSpec {
-    name string
-    display_name string
-}
-
-struct Interpreter {
-    hash string
-}
-
-struct Metadata {
-    orig_nbformat int
-    language_info LangInfo
-    kernelspec KernelSpec
-    interpreter Interpreter
-}
-
-struct OutputData {
-    text_plain []string [json:'text/plain']
-    text_html []string [json:'text/html']
-}
-
-struct Output {
-    output_type string
-    data OutputData
-    metadata Metadata
-    execution_count int
-}
-
-struct Cell {
-    cell_type string
-    execution_count int
-    source []string
-    outputs []Output
-    metadata Metadata
-}
-
-struct IPynb {
-    cells []Cell
-    metadata Metadata
-    nbformat int
-    nbformat_minor int
-}
+module main
 
 fn main() {
-    s := os.read_file('./sample/sample.ipynb') or {
-	eprintln('Failed to open file')
-	return
-    }
-    mut ipynb := json.decode(IPynb, s) or {
-	eprintln('Failed to parse json')
+    ipynb := parse_ipynb('./sample/sample.ipynb') or {
 	return
     }
     for cell in ipynb.cells {
